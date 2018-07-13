b832a612cfe9f4f510307570f06666494ee9575f
Leonardo Tanenbaum-diaz <ltanenba@e1z1r6p18.42.us.org>
2018-07-12 22:52:34 -0700

    modify ft_itoa

--- src/add_funcs/ft_itoa.c
+++ src/add_funcs/ft_itoa.c
@@ -6,41 +6,34 @@
 /*   By: ltanenba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 15:33:26 by ltanenba          #+#    #+#             */
-/*   Updated: 2018/06/09 00:41:36 by ltanenba         ###   ########.fr       */
+/*   Updated: 2018/07/12 22:39:23 by ltanenba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	get_str_len(int n)
+char	*ft_itoa(int n)
 {
-	size_t		i;
+	int		numlen;
+	long	nc;
+	char	*str;
 
-	i = 1;
-	while (n /= 10)
-		i++;
-	return (i);
-}
-
-char			*ft_itoa(int n)
-{
-	char			*str;
-	size_t			str_len;
-	unsigned int	n_cpy;
-
-	str_len = get_str_len(n);
-	n_cpy = n;
-	if (n < 0)
+	numlen = ft_numlen(n);
+	nc = (long)n;
+	str = ft_strnew(numlen);
+	if (str == NULL)
+		return (NULL);
+	str[numlen--] = '\0';
+	if (nc < 0)
 	{
-		n_cpy = -n;
-		str_len++;
+		str[0] = '-';
+		nc = -nc;
 	}
-	if (!(str = ft_strnew(str_len)))
-		return (0);
-	str[--str_len] = n_cpy % 10 + '0';
-	while (n_cpy /= 10)
-		str[--str_len] = n_cpy % 10 + '0';
-	if (n < 0)
-		*(str + 0) = '-';
+	str[numlen--] = (nc % 10) + '0';
+	while (nc >= 10)
+	{
+		nc /= 10;
+		str[numlen--] = (nc % 10) + '0';
+	}
 	return (str);
 }
